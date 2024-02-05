10f169e7af49ba3567128dd794b1521f436628d4
MrBoboGet <emanuelberggren01@gmail.com>
2024-02-06 00:19:01 +0100

    Removed temporary definitions

--- src/Server/Handler.hs
+++ src/Server/Handler.hs
@@ -53,30 +53,22 @@ import           Data.Text                      ( Text
                                                 )
 import qualified Data.Text                     as Text
 import           Language.LSP.Server            ( LspM )
-import qualified Language.LSP.Server           as LSP
-import qualified Language.LSP.Types            as LSP
 import qualified Language.LSP.VFS              as VFS
 import           Monad
 import           Options                        ( Config
                                                 , Options(optRawAgdaOptions)
                                                 )
+import qualified Language.LSP.Server           as LSP
+import qualified Language.LSP.Types            as LSP
+import qualified Server.Index as Index
+
 
 import qualified Data.Strict as Strict 
 import qualified Data.Map as Map 
 import qualified Language.LSP.Types as LSPTypes 
-import Data.List
-import Agda.Interaction.Imports
-import Agda.Syntax.TopLevelModuleName
 import Agda.Utils.FileName
 import Agda.Interaction.FindFile
-import Agda.Interaction.Highlighting.FromAbstract
-import qualified Agda.Interaction.Highlighting.Precise as P
-import Agda.Utils.RangeMap
-import Agda.Syntax.Abstract.Name
-import Agda.Syntax.Scope.Base
 import qualified Agda.Syntax.Common.Aspect as Aspect
-import Agda.Interaction.Highlighting.Range as A
-import qualified Server.Index as Index
 import qualified Language.LSP.Server as LSPServer
 
 
@@ -84,15 +76,11 @@ import qualified Agda.TypeChecking.Errors as TCM
 import qualified Agda.TypeChecking.Monad  as TCM
 import qualified Agda.TypeChecking.Pretty as TCM
 import qualified Agda.Interaction.Highlighting.Generate as G
-import qualified Agda.Utils.Impossible as IM
-import qualified Data.IntMap as IntMap
 import Agda.Utils.List            ( caseList, last1 )
 
 import qualified Server.Highlight as H
-import qualified Agda.Syntax.Concrete as C
-import qualified Agda.Syntax.Abstract as Abs
 import qualified Agda.Interaction.Base as CM
-import Data.Sequence
+import qualified Agda.Interaction.Highlighting.Precise as P
 
 initialiseCommandQueue :: IO CommandQueue
 initialiseCommandQueue = CommandQueue <$> newTChanIO <*> newTVarIO Nothing
@@ -135,62 +123,6 @@ inferTypeOfText filepath text = runCommandM $ do
 
   render <$> prettyATop typ
 
-    
-
-getAbstract :: FilePath -> ServerM (LspM Config) (Either String (TopLevelModuleName,Index.LineIndex  , TopLevelInfo))
-getAbstract filepath = runCommandM $ do
-  -- localStateCommandM: restore TC state afterwards, do we need this here?
-  (n,i,t) <- localStateCommandM $ do
-    fp <- liftIO $ absolute filepath
-    s <- lift $ parseSource (SourceFile fp)
-    index <- liftIO $ Index.fileToLineIndex filepath
-    lift $ atTopLevel $ do 
-            t <- concreteToAbstract_ (TopLevel fp (srcModuleName s) ([] :: [Declaration]) )
-            return (srcModuleName s , index , t)
-    --lift $ atTopLevel $ do 
-    --        t <- concreteToAbstract_ (TopLevel fp (srcModuleName s) (modDecls (srcModule s)))
-    --        return (srcModuleName s , index , t)
-  return (n,i,t)
-
-getHighlightFile :: FilePath -> ServerM (LspM Config) (Either String (Index.LineIndex , [(A.Range , P.Aspects)]))
-getHighlightFile filepath = runCommandM $ do
-         fp <- liftIO $ absolute filepath
-         s <- lift $ parseSource (SourceFile fp)
-         highlight <- lift $ atTopLevel $ do (getHighlightSource s)
-         index <- liftIO $ Index.fileToLineIndex filepath
-         return (index, highlight)
-
---getSemanticTokens :: 
-
---kindMap :: KindOfName -> NameKind
---kindMap ConName = Constructor Aspect.Inductive
---kindMap CoConName = Constructor Aspect.CoInductive
---kindMap FldName = Aspect.Record
---kindMap PatternSynName = Aspect.Function
---kindMap GeneralizeName = Aspect.Generalizable
---kindMap DisallowedGeneralizeName = Aspect.Generalizable
---kindMap MacroName = Aspect.Macro
---kindMap QuotableName = Aspect.Field
---kindMap DataName = Aspect.Record
---kindMap RecName = Aspect.Record
---kindMap FunName = Aspect.Function
---kindMap AxiomName = Aspect.Postulate
---kindMap PrimName = Aspect.Primitive
---kindMap OtherDefName = Aspect.Argument
-
-
-nameLookup :: NameMap -> (QName -> Maybe P.NameKind)
-nameLookup m x = case Map.lookup x m of 
-                Nothing -> Nothing 
-                Just a -> Just (P.kindOfNameToNameKind (qnameKind a))
-getHighlightInfo :: TopLevelInfo -> TopLevelModuleName -> RangeMap P.Aspects
-getHighlightInfo top@(TopLevelInfo decls scope) name = P.convert (runHighlighter name (nameLookup (_scopeInverseName scope)) decls)
-
-getHighlightSource :: Source -> TCM.TCM [(A.Range , P.Aspects)]
-getHighlightSource s = do
-                         (CheckResult interface _ _ _) <- (typeCheckMain ScopeCheck s)
-                         return (toList $ TCM.iHighlighting interface)
-
 onHover :: LSP.Uri -> LSP.Position -> ServerM (LspM Config) (Maybe LSP.Hover)
 onHover uri pos = do
   result <- LSP.getVirtualFile (LSP.toNormalizedUri uri)
@@ -223,133 +155,6 @@ onHover uri pos = do
                   return $ Just $ LSP.Hover content (Just range)
 
 
-lineLength :: APosition.Interval ->  LSP.UInt
-lineLength (APosition.Interval p1 p2) = fromInteger $ fromIntegral (APosition.posCol p2 - APosition.posCol p1)
-
-convertTokenType :: Token -> LSP.SemanticTokenTypes 
-convertTokenType (TokKeyword _ _) = LSP.SttNamespace
-convertTokenType (TokId (_ , _)) = LSP.SttVariable
-convertTokenType (TokQId _) = LSP.SttVariable
-convertTokenType (TokLiteral _) = LSP.SttString
-convertTokenType (TokSymbol _ _) = LSP.SttClass
-convertTokenType (TokString _) = LSP.SttString
-convertTokenType (TokTeX (_ , _)) = LSP.SttComment
-convertTokenType (TokMarkup (_ , _)) = LSP.SttComment
-convertTokenType (TokComment (_ , _)) = LSP.SttComment
-convertTokenType TokDummy = LSP.SttNamespace
-convertTokenType (TokEOF _) = LSP.SttNamespace
-
-tokenInterval :: Token -> APosition.Interval
-tokenInterval (TokKeyword _ i) = i
-tokenInterval (TokId (i , _)) = i
-tokenInterval (TokQId (head : tail)) = fst head
-tokenInterval (TokQId []) = APosition.Interval   (APosition.Pn Strict.Nothing 0 0 0)  (APosition.Pn Strict.Nothing 0 0 0)
-tokenInterval (TokLiteral _) = APosition.Interval   (APosition.Pn Strict.Nothing 0 0 0)  (APosition.Pn Strict.Nothing 0 0 0)
-tokenInterval (TokSymbol _ i) = i
-tokenInterval (TokString (i , _)) = i
-tokenInterval (TokTeX (i , _)) = i
-tokenInterval (TokMarkup (i , _)) = i
-tokenInterval (TokComment (i , _)) = i
-tokenInterval TokDummy = APosition.Interval   (APosition.Pn Strict.Nothing 0 0 0)  (APosition.Pn Strict.Nothing 0 0 0)
-tokenInterval (TokEOF i) = i
-
-tokenLength :: Token -> LSP.UInt
-tokenLength t = lineLength $ tokenInterval t
-
-tokenLineStart :: Token -> LSP.UInt
-tokenLineStart t = (fromIntegral $ APosition.posLine (APosition.iStart $ tokenInterval t)) - fromIntegral 1
-tokenColStart :: Token -> LSP.UInt
-tokenColStart t = (fromIntegral $ APosition.posCol (APosition.iStart $ tokenInterval t)) - fromIntegral 1
-
-convertTokens :: (LSP.SemanticTokenTypes -> LSP.UInt) -> [Token] -> LSP.Position -> LSP.List LSP.UInt
-convertTokens f [] (LSP.Position l  r) = LSP.List []
-convertTokens f (head : tail) (LSP.Position l  r) = LSP.List [ tokenLineStart head - l , tokenColStart head - r , 2 , f $ convertTokenType head , 0] <> convertTokens f tail (LSP.Position (tokenLineStart head) (tokenColStart head))
-
-
---defaultMap :: Map.Map;
---defaultMap = Map.fromList (LSPTypes.SemanticTokensLegend)
-
-defaultTokenMap :: LSP.SemanticTokenTypes -> LSP.UInt
-defaultTokenMap a = fromIntegral $ case (elemIndex a LSP.knownSemanticTokenTypes) of
-                                        Nothing -> 0
-                                        Just i -> i
-
-
---type ASTInfo = [(LSP.Position , Aspects)]
---convertHighlightInfo :: Index.LineIndex -> RangeMap Aspects -> ASTInfo
---convertHighlightInfo index rangeMap = do 
---                                        a <- toList rangeMap
---                                        b <- map (\((A.Range start end),content) -> ((LSP.Position (fromIntegral start) (fromIntegral end)) , content)) a
---                                        []
-
-nameKindToSemantic :: P.NameKind -> LSP.SemanticTokenTypes
-nameKindToSemantic (P.Constructor _) = LSP.SttMacro
-nameKindToSemantic P.Datatype = LSP.SttClass
-nameKindToSemantic P.Field = LSP.SttVariable
-nameKindToSemantic P.Module = LSP.SttNamespace
-nameKindToSemantic P.Postulate = LSP.SttMacro
-nameKindToSemantic P.Primitive = LSP.SttClass
-nameKindToSemantic P.Record = LSP.SttMacro
-nameKindToSemantic P.Argument = LSP.SttVariable
-nameKindToSemantic P.Macro = LSP.SttMacro
-nameKindToSemantic _ = LSP.SttFunction
-
-aspectToSemantic :: P.Aspect -> LSP.SemanticTokenTypes
-aspectToSemantic P.Comment = LSP.SttComment
-aspectToSemantic P.Keyword = LSP.SttKeyword
-aspectToSemantic P.String = LSP.SttString
-aspectToSemantic P.Number = LSP.SttNumber
-aspectToSemantic P.Hole = LSP.SttString
-aspectToSemantic P.Symbol = LSP.SttVariable
-aspectToSemantic P.PrimitiveType = LSP.SttClass
-aspectToSemantic (P.Name Nothing _) = LSP.SttClass
-aspectToSemantic (P.Name (Just kind) _) = nameKindToSemantic kind
-aspectToSemantic P.Pragma = LSP.SttComment
-aspectToSemantic P.Background = LSP.SttComment
-aspectToSemantic P.Markup = LSP.SttComment
-
-convertRangeTokens :: Int -> Int -> Index.LineIndex -> (LSP.SemanticTokenTypes -> LSP.UInt) -> [(A.Range , P.Aspects)] -> LSP.List LSP.UInt
-convertRangeTokens prevLine prevCol index map ( (A.Range start end , a) : tail) = case P.aspect a of 
-    Nothing -> convertRangeTokens prevLine prevCol index map tail
-    Just asp -> do 
-                  let (LSP.Position line col) = Index.offsetToPosition index start
-                  let (LSP.Position endLine endCol) = Index.offsetToPosition index end
-                  let deltaCol = if line == fromIntegral prevLine  then  col - (fromIntegral prevCol) else col
-                  let length = if endCol > col  then  endCol - col else (if line /= endLine then 100+endLine else 1000+line)
-                  (LSP.List 
-                    [ line - (fromIntegral prevLine) , deltaCol , length , map $ aspectToSemantic asp, 0]) 
-                    <> convertRangeTokens (fromIntegral line) (fromIntegral col) index map tail
-convertRangeTokens line col index map [] = LSP.List []
-
-getDeclarationHighlight :: Abs.Declaration -> ServerM (LspM Config) (Either String [(APosition.Range,P.Aspects)])
-getDeclarationHighlight decl = runCommandM $  do
-    result <- lift $ atTopLevel $ H.generateSyntaxInfo decl False
-    return result
-
-getTopHighlightInfo :: [Abs.Declaration] -> ServerM (LspM Config) [(APosition.Range,P.Aspects)]
-getTopHighlightInfo [] = do return []
-getTopHighlightInfo (head : tail) = do 
-                                declHighlight <- getDeclarationHighlight head
-                                rest <- getTopHighlightInfo tail
-                                case declHighlight of 
-                                    Left _ -> return rest
-                                    Right headTokens -> return (headTokens <> rest)
-
-
-getWriteContent :: [(A.Range , P.Aspects)] -> String
-getWriteContent input = foldl (<>) "" (map (\((A.Range x y),_) -> (show x) <> " " <> (show y) <> "\n") input)
-writeResult :: String -> String -> IO ()
-writeResult file input = do
-                writeFile file input
-                return ()
-
-writeIndexResult :: Index.LineIndex -> [(A.Range , P.Aspects)] -> String
-writeIndexResult index aspects = foldl (<>) "" (map (\((LSP.Position x y),(LSP.Position x2 y2)) -> "(" <> (show x) <> "," <> (show y)<>") ("<> (show x2) <> "," <> (show y2) <> ")\n")  ((map (\((A.Range x y) , _) -> ((Index.offsetToPosition index x),(Index.offsetToPosition index y))))   aspects))
-
-writeTokensResult :: LSP.List LSP.UInt -> String
-writeTokensResult (LSP.List (x1:x2:x3:x4:x5:tail)) = (foldl (\x y -> x <> " " <> (show y)) "" [x1,x2,x3,x4,x5]) <> "\n" <> 
-    (writeTokensResult (LSP.List tail))
-writeTokensResult _ = ""
 
 
 onHighlight :: LSP.Uri -> ServerM (LspM Config) (Maybe LSP.SemanticTokens)
@@ -365,10 +170,10 @@ onHighlight ur =
           cm <- get
           let file = CM.theCurrentFile cm
           
-          (stringToPrint, indexString , tokenResult,tokenReturn) <- case file of 
+          tokenReturn <- case file of 
             Nothing -> do
                 --LSPServer.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError (pack "no current file :((("))
-                return ("","","",(Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttVariable ,0]) )))
+                return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,H.defaultTokenMap LSP.SttVariable ,0]) ))
             Just content -> do
                 let persistentState = TCM.stPersistentState tc
                 let modName = CM.currentFileModule content
@@ -376,38 +181,21 @@ onHighlight ur =
                 case moduleInfo of
                     Nothing -> do
                         --LSPServer.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError (pack "no current file :((("))
-                        return ("","","",(Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttClass ,0]) )))
+                        return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,H.defaultTokenMap LSP.SttClass ,0])))
                     Just content -> do
                         --LSPServer.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError (pack "no current file :((("))
                         let highlight = TCM.iHighlighting $ TCM.miInterface content
-                        let listTokens = toList highlight
-                        let semTokensContet = convertRangeTokens 0 0 index defaultTokenMap listTokens
+                        let listTokens = P.toList highlight
+                        let semTokensContet = H.convertRangeTokens 0 0 index H.defaultTokenMap listTokens
                         let semTokens = (LSP.SemanticTokens Nothing semTokensContet)
-                        return (getWriteContent listTokens, writeIndexResult index listTokens, writeTokensResult semTokensContet, (Just semTokens ))
-          liftIO (writeResult "C:/Users/emanu/Desktop/Program/Haskell/agda-language-server/offset.txt" stringToPrint)
-          liftIO (writeResult "C:/Users/emanu/Desktop/Program/Haskell/agda-language-server/index.txt" indexString)
-          liftIO (writeResult "C:/Users/emanu/Desktop/Program/Haskell/agda-language-server/sem.txt" tokenResult)
+                        return  (Just semTokens)
           return tokenReturn
       case result of 
            Left s -> do
                   LSPServer.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError (pack s))
-                  return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttMacro ,0]) ))
+                  return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,H.defaultTokenMap LSP.SttMacro ,0]) ))
            Right tokens -> do
                   return tokens
---return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttClass ,0]) ))
-      ----return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttClass ,0]) ))
-      --case result of
-      --  Nothing   -> return Nothing
-      --  Just file -> do
-      --    let source      = VFS.virtualFileText file
-      --    --return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttClass ,0]) ))
-      --    mAbstract <- getAbstract path 
-      --    case mAbstract of
-      --      Left s -> do
-      --                  LSPServer.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError (pack s))
-      --                  return (Just (LSP.SemanticTokens Nothing (LSP.List [0,0,5,defaultTokenMap LSP.SttClass ,0]) ))
-      --      Right (n,index,ast) -> do
-      --            return (Just (LSP.SemanticTokens Nothing (convertRangeTokens 0 0 index defaultTokenMap (toList (getHighlightInfo ast n)))))
 --------------------------------------------------------------------------------
 -- Helper functions for converting stuff to SemanticTokenAbsolute
 
--- src/Server/Highlight.hs
+++ src/Server/Highlight.hs
@@ -1,201 +1,69 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 module Server.Highlight where
+import qualified Agda.Interaction.Highlighting.Precise as P
+import qualified Language.LSP.Server           as LSP
+import qualified Language.LSP.Types            as LSP
+import qualified Server.Index as Index
+import Agda.Interaction.Highlighting.Range as A
+import Data.List
 
-import Prelude hiding (null)
-
-import Control.Monad
-
-import qualified Data.Foldable as Fold
-import qualified Data.Map as Map
-import Data.Maybe
-import Data.List ((\\))
-import qualified Data.List as List
-import qualified Data.IntMap as IntMap
-import Data.Semigroup (Semigroup(..))
-import Data.Sequence (Seq)
-import qualified Data.Set as Set
-import qualified Data.Text.Lazy as Text
-
-import Agda.Interaction.Response
-  ( RemoveTokenBasedHighlighting( KeepHighlighting ) )
-import Agda.Interaction.Highlighting.Precise as H
-import Agda.Interaction.Highlighting.Range
-  (rToR, rangeToRange, overlappings, Ranges)
-import Agda.Interaction.Highlighting.FromAbstract
-
-import qualified Agda.TypeChecking.Errors as TCM
-import Agda.TypeChecking.MetaVars (isBlockedTerm, hasTwinMeta)
-import Agda.TypeChecking.Monad
-  hiding (ModuleInfo, MetaInfo, Primitive, Constructor, Record, Function, Datatype)
-import qualified Agda.TypeChecking.Monad  as TCM
-import qualified Agda.TypeChecking.Monad.Base.Warning as W
-import qualified Agda.TypeChecking.Pretty as TCM
-import Agda.TypeChecking.Positivity.Occurrence
-import Agda.TypeChecking.Warnings ( raiseWarningsOnUsage, runPM )
-
-import qualified Agda.Syntax.Abstract as A
-import Agda.Syntax.Concrete.Definitions as W ( DeclarationWarning(..), DeclarationWarning'(..) )
-import qualified Agda.Syntax.Concrete.Name as C
-import qualified Agda.Syntax.Internal as I
-import qualified Agda.Syntax.Literal as L
-import qualified Agda.Syntax.Parser as Pa
-import qualified Agda.Syntax.Parser.Tokens as T
-import qualified Agda.Syntax.Position as P
-import Agda.Syntax.Position
-  (RangeFile, Range, HasRange, getRange, noRange)
-import Agda.Syntax.TopLevelModuleName
-
-import Agda.Syntax.Scope.Base     ( WithKind(..) )
-import Agda.Syntax.Abstract.Views ( KName, declaredNames )
-
-import Agda.Utils.FileName
-import Agda.Utils.List            ( caseList, last1 )
-import Agda.Utils.List2           ( List2 )
-import qualified Agda.Utils.List2 as List2
-import Agda.Utils.Maybe
-import qualified Agda.Utils.Maybe.Strict as Strict
-import Agda.Utils.Null
-import Agda.Syntax.Common.Pretty
-import Agda.Utils.Singleton
-
-import Agda.Utils.Impossible
-
-import qualified Agda.Interaction.Highlighting.Generate as G
-
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HMap
-import qualified Agda.Syntax.Common.Aspect as ASP
-
-generateSyntaxInfo
-  :: A.Declaration
-       -- ^ Declaration to highlight.
-  -> Bool
-       -- ^ Update the state?
-  -> TCM [(Range , ASP.Aspects)]
-generateSyntaxInfo decl _ | null $ getRange decl = return []
-generateSyntaxInfo decl updateState = let hlLevel = G.Partial  in do
-  top <- fromMaybe __IMPOSSIBLE__ <$> currentTopLevelModule
-  ignoreAbstractMode $ do
-    kinds <- nameKinds hlLevel decl
-
-    -- After the code has been type checked more information may be
-    -- available for overloaded constructors, and
-    -- @generateConstructorInfo@ takes advantage of this information.
-    -- Note, however, that highlighting for overloaded constructors is
-    -- included also in @nameInfo@.
-    constructorInfo <- case hlLevel of
-      G.Full{} -> generateConstructorInfo top kinds decl
-      _      -> return mempty
-
-    -- Main source of scope-checker generated highlighting:
-    let nameInfo = runHighlighter top kinds decl
-    -- Highlighting from the lexer and Happy parser:
-    (curTokens, otherTokens) <-
-      insideAndOutside (rangeToRange (getRange decl)) <$> useTC stTokens
-
-    -- @constructorInfo@ needs
-    -- to be placed before @nameInfo@ since, when typechecking is done,
-    -- constructors are included in both lists. Finally the token
-    -- information is placed last since token highlighting is more
-    -- crude than the others.
-    let syntaxInfo = convert (constructorInfo <> nameInfo)
-                       <>
-                     curTokens
-
-    when updateState $ do
-      stSyntaxInfo `modifyTCLens` mappend syntaxInfo
-      stTokens     `setTCLens`    otherTokens
-
-    return []
-generateConstructorInfo
-  :: TopLevelModuleName
-     -- ^ The module to highlight.
-  -> NameKinds
-  -> A.Declaration
-  -> TCM HighlightingInfoBuilder
-generateConstructorInfo top kinds decl = do
-
-  -- Get boundaries of current declaration.
-  -- @noRange@ should be impossible, but in case of @noRange@
-  -- it makes sense to return mempty.
-  caseList (P.rangeIntervals $ getRange decl)
-           (return mempty) $ \ i is -> do
-    let start = fromIntegral $ P.posPos $ P.iStart i
-        end   = fromIntegral $ P.posPos $ P.iEnd $ last1 i is
-
-    -- Get all disambiguated names that fall within the range of decl.
-    m0 <- useTC stDisambiguatedNames
-    let (_, m1) = IntMap.split (pred start) m0
-        (m2, _) = IntMap.split end m1
-        constrs = IntMap.elems m2
-
-    -- Return suitable syntax highlighting information.
-    return $ foldMap (runHighlighter top kinds) constrs
+defaultTokenMap :: LSP.SemanticTokenTypes -> LSP.UInt
+defaultTokenMap a = fromIntegral $ case (elemIndex a LSP.knownSemanticTokenTypes) of
+                                        Nothing -> 0
+                                        Just i -> i
 
 
+nameKindToSemantic :: P.NameKind -> LSP.SemanticTokenTypes
+nameKindToSemantic (P.Constructor _) = LSP.SttMacro
+nameKindToSemantic P.Datatype = LSP.SttClass
+nameKindToSemantic P.Field = LSP.SttVariable
+nameKindToSemantic P.Module = LSP.SttNamespace
+nameKindToSemantic P.Postulate = LSP.SttMacro
+nameKindToSemantic P.Primitive = LSP.SttClass
+nameKindToSemantic P.Record = LSP.SttMacro
+nameKindToSemantic P.Argument = LSP.SttVariable
+nameKindToSemantic P.Macro = LSP.SttMacro
+nameKindToSemantic _ = LSP.SttFunction
 
-nameKinds :: G.Level
-             -- ^ This should only be @'Full'@ if
-             -- type-checking completed successfully (without any
-             -- errors).
-          -> A.Declaration
-          -> TCM NameKinds
-nameKinds hlLevel decl = do
-  imported <- useTC $ stImports . sigDefinitions
-  local    <- case hlLevel of
-    G.Full{} -> useTC $ stSignature . sigDefinitions
-    _      -> return HMap.empty
-  impPatSyns <- useTC stPatternSynImports
-  locPatSyns <- case hlLevel of
-    G.Full{} -> useTC stPatternSyns
-    _      -> return empty
-      -- Traverses the syntax tree and constructs a map from qualified
-      -- names to name kinds. TODO: Handle open public.
-  let syntax :: NameKindMap
-      syntax = runBuilder (declaredNames decl :: NameKindBuilder) HMap.empty
-  return $ \ n -> unionsMaybeWith mergeNameKind
-    [ defnToKind . theDef <$> HMap.lookup n local
-    , con <$ Map.lookup n locPatSyns
-    , defnToKind . theDef <$> HMap.lookup n imported
-    , con <$ Map.lookup n impPatSyns
-    , HMap.lookup n syntax
-    ]
-  where
-  defnToKind :: TCM.Defn -> NameKind
-  defnToKind   TCM.Axiom{}                           = Postulate
-  defnToKind   TCM.DataOrRecSig{}                    = Postulate
-  defnToKind   TCM.GeneralizableVar{}                = Generalizable
-  defnToKind d@TCM.Function{} | isProperProjection d = Field
-                            | otherwise            = Function
-  defnToKind   TCM.Datatype{}                        = Datatype
-  defnToKind   TCM.Record{}                          = Record
-  defnToKind   TCM.Constructor{ TCM.conSrcCon = c }  = Constructor $ I.conInductive c
-  defnToKind   TCM.Primitive{}                       = Primitive
-  defnToKind   TCM.PrimitiveSort{}                   = Primitive
-  defnToKind   TCM.AbstractDefn{}                    = __IMPOSSIBLE__
+aspectToSemantic :: P.Aspect -> LSP.SemanticTokenTypes
+aspectToSemantic P.Comment = LSP.SttComment
+aspectToSemantic P.Keyword = LSP.SttKeyword
+aspectToSemantic P.String = LSP.SttString
+aspectToSemantic P.Number = LSP.SttNumber
+aspectToSemantic P.Hole = LSP.SttString
+aspectToSemantic P.Symbol = LSP.SttVariable
+aspectToSemantic P.PrimitiveType = LSP.SttClass
+aspectToSemantic (P.Name Nothing _) = LSP.SttClass
+aspectToSemantic (P.Name (Just kind) _) = nameKindToSemantic kind
+aspectToSemantic P.Pragma = LSP.SttComment
+aspectToSemantic P.Background = LSP.SttComment
+aspectToSemantic P.Markup = LSP.SttComment
 
-  con :: NameKind
-  con = Constructor ASP.Inductive
-type NameKindMap     = HashMap A.QName NameKind
-
-data NameKindBuilder = NameKindBuilder
-  { runBuilder :: NameKindMap -> NameKindMap
-  }
-
-mergeNameKind :: NameKind -> NameKind -> NameKind
-mergeNameKind Postulate k = k
-mergeNameKind _     Macro = Macro  -- If the abstract syntax says macro, it's a macro.
-mergeNameKind k         _ = k
+convertRangeTokens :: Int -> Int -> Index.LineIndex -> (LSP.SemanticTokenTypes -> LSP.UInt) -> [(A.Range , P.Aspects)] -> LSP.List LSP.UInt
+convertRangeTokens prevLine prevCol index map ( (A.Range start end , a) : tail) = case P.aspect a of 
+    Nothing -> convertRangeTokens prevLine prevCol index map tail
+    Just asp -> do 
+                  let (LSP.Position line col) = Index.offsetToPosition index start
+                  let (LSP.Position endLine endCol) = Index.offsetToPosition index end
+                  let deltaCol = if line == fromIntegral prevLine  then  col - (fromIntegral prevCol) else col
+                  let length = if endCol > col  then  endCol - col else (if line /= endLine then 100+endLine else 1000+line)
+                  (LSP.List 
+                    [ line - (fromIntegral prevLine) , deltaCol , length , map $ aspectToSemantic asp, 0]) 
+                    <> convertRangeTokens (fromIntegral line) (fromIntegral col) index map tail
+convertRangeTokens line col index map [] = LSP.List []
 
 
+getWriteContent :: [(A.Range , P.Aspects)] -> String
+getWriteContent input = foldl (<>) "" (map (\((A.Range x y),_) -> (show x) <> " " <> (show y) <> "\n") input)
+writeResult :: String -> String -> IO ()
+writeResult file input = do
+                writeFile file input
+                return ()
 
-instance Semigroup (NameKindBuilder) where
-  NameKindBuilder f <> NameKindBuilder g = NameKindBuilder $ f . g
-instance Monoid (NameKindBuilder) where
-  mempty = NameKindBuilder id
-  mappend = (<>)
-instance Singleton KName NameKindBuilder where
-  singleton (WithKind k q) = NameKindBuilder $
-    HMap.insertWith mergeNameKind q $ kindOfNameToNameKind k
-instance Collection KName NameKindBuilder
+writeIndexResult :: Index.LineIndex -> [(A.Range , P.Aspects)] -> String
+writeIndexResult index aspects = foldl (<>) "" (map (\((LSP.Position x y),(LSP.Position x2 y2)) -> "(" <> (show x) <> "," <> (show y)<>") ("<> (show x2) <> "," <> (show y2) <> ")\n")  ((map (\((A.Range x y) , _) -> ((Index.offsetToPosition index x),(Index.offsetToPosition index y))))   aspects))
+
+writeTokensResult :: LSP.List LSP.UInt -> String
+writeTokensResult (LSP.List (x1:x2:x3:x4:x5:tail)) = (foldl (\x y -> x <> " " <> (show y)) "" [x1,x2,x3,x4,x5]) <> "\n" <> 
+    (writeTokensResult (LSP.List tail))
+writeTokensResult _ = ""
+
